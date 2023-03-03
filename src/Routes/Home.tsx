5d06bc733940e5b9001dd00d277ceea1b75f3518
jiwoon-code <115874237+jiwoon-code@users.noreply.github.com>
2023-03-04 02:02:06 +0900

    home screen img api

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,5 +1,59 @@
+import styled from "styled-components";
+import { useQuery } from "@tanstack/react-query";
+import { getMovies, IGetMoviesResult } from "../api";
+import { makeImgPath } from "../Utills";
+
+const Wrapper = styled.div`
+  background: black;
+`;
+
+const Loader = styled.div`
+  height: 20vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Banner = styled.div<{ bgPhoto: string }>`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 60px;
+  background-image: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)),
+    url(${(props) => props.bgPhoto});
+  background-size: cover;
+`;
+
+const Title = styled.h2`
+  font-size: 50px;
+  margin-bottom: 20px;
+`;
+const Overview = styled.p`
+  font-size: 25px;
+  width: 50%;
+`;
+
 function Home() {
-  return <div style={{ backgroundColor: "white", height: "200vh" }}></div>;
+  const { data, isLoading } = useQuery<IGetMoviesResult>(
+    ["movies", "nowPlaying"],
+    getMovies
+  );
+  console.log(data, isLoading);
+  return (
+    <Wrapper>
+      {isLoading ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        <>
+          <Banner bgPhoto={makeImgPath(data?.results[0].backdrop_path || "")}>
+            <Title>{data?.results[0].title}</Title>
+            <Overview>{data?.results[0].overview}</Overview>
+          </Banner>
+        </>
+      )}
+    </Wrapper>
+  );
 }
 
 export default Home;
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RecoilRoot } from "recoil";
 import { ThemeProvider } from "styled-components";
 import App from "./App";
@@ -61,9 +62,9 @@ table {
 body {
   font-weight: 300;
   font-family: 'Source Sans Pro', sans-serif;
-  color:black;
+  color:${(props) => props.theme.white.darker};
   line-height: 1.2;
- 
+ background-color: black;
 }
 a {
   text-decoration:none;
@@ -71,12 +72,16 @@ a {
 }
 `;
 
+const client = new QueryClient();
+
 ReactDOM.render(
   <RecoilRoot>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <App />
-    </ThemeProvider>
+    <QueryClientProvider client={client}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
+    </QueryClientProvider>
   </RecoilRoot>,
 
   document.getElementById("root")
